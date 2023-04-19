302612a12dfa75c27c7442c14096c1c1b8a34b77
Nauriz <tasbula03@mail.ru>
2023-04-19 22:05:59 +0600

    some functions added

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,35 +1,60 @@
-public class MyArrayList implements MyList{
-    private Object T[];
+import java.util.Arrays;
 
+public class MyArrayList<T> implements MyList<T>{
+    private static final int INITIAL_CAPACITY = 10;
+    private Object[] array;
     private int size;
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean contains(Object o) {
+        for (int i = 0; i < size; i++){
+            if (array[i].equals(o)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public void add(Object item) {
-
+    public void add(T item) {
+        if (size == array.length) {
+            array = Arrays.copyOf(array, array.length * 2);
+        }
+        array[size++] = item;
     }
 
     @Override
     public void add(Object item, int index) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        if (size == array.length) {
+            array = Arrays.copyOf(array, array.length * 2);
+        }
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = item;
+        size++;
     }
 
     @Override
     public boolean remove(Object item) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(item)) {
+                System.arraycopy(array, i + 1, array, i, size - i - 1);
+                size--;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public Object remove(int index) {
+    public T remove(int index) {
         return null;
     }
 
@@ -39,7 +64,7 @@ public class MyArrayList implements MyList{
     }
 
     @Override
-    public Object get(int index) {
+    public T get(int index) {
         return null;
     }
 
