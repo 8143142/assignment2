e08f7e2280d1e1c80a63fe529975d228ef9293ac
Nauriz <tasbula03@mail.ru>
2023-04-19 23:26:28 +0600

    Added function

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -15,5 +15,13 @@ public class MyLinkedList<T> implements MyList<T> {
     private Node tail;
     private int size;
 
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
 
+    public int size() {
+        return size;
+    }
 }
