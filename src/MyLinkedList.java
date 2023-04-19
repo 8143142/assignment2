0e88422576cf26a674093431a3fb4ced90c4eda9
Nauriz <tasbula03@mail.ru>
2023-04-19 23:34:11 +0600

    Added function add

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,57 +1,58 @@
 public class MyLinkedList<T> implements MyList<T> {
-private class Node {
+    private class Node {
         T element;
         Node next;
         Node prev;
 
-public Node(T element) {
-        this.element = element;
-        this.next = null;
-        this.prev = null;
+        public Node(T element) {
+            this.element = element;
+            this.next = null;
+            this.prev = null;
         }
-        }
+    }
 
-private Node head;
-private Node tail;
-private int size;
+    private Node head;
+    private Node tail;
+    private int size;
 
-public MyLinkedList() {
+    public MyLinkedList() {
         head = null;
         tail = null;
         size = 0;
-        }
+    }
 
-public int size() {
+    public int size() {
         return size;
-        }
+    }
 
-public boolean contains(Object o) {
-        return indexOf(o)!= -1;
-        }
+    public boolean contains(Object o) {
+        return indexOf(o) != -1;
+    }
 
-public void add(T item) {
+    public void add(T item) {
         Node newNode = new Node(item);
         if (tail == null) {
-        head = newNode;
-        tail = newNode;
+            head = newNode;
+            tail = newNode;
         } else {
-        tail.next = newNode;
-        newNode.prev = tail;
-        tail = newNode;
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
         }
         size++;
-        }
+    }
 
-public void add(T item, int index) {
-    if (index < 0 || index > size) {
-        throw new IndexOutOfBoundsException();
-    }
-    if (index == size) {
-        add(item);
-        return;
-    }
-    Node newNode = new Node(item);
-    if (index == 0) {
-        newNode.next = head;
+    public void add(T item, int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == size) {
+            add(item);
+            return;
+        }
+        Node newNode = new Node(item);
+        if (index == 0) {
+            newNode.next = head;
+        }
     }
 }
