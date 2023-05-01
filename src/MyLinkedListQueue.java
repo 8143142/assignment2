176bac8b8df1cefed75196cfa0d91367c3c16fef
Nauriz <tasbula03@mail.ru>
2023-05-02 00:49:11 +0600

    added method dequeue

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -10,4 +10,16 @@ public class MyLinkedListQueue<T> {
     public void enqueue(T item) {
         list.addLast(item);
     }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list.removeFirst();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
 }
